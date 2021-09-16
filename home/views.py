873b39d8063d37576d6b786aaa0b9b80fcafaa9d
Rajabu Mrisho <75034394+Raj067@users.noreply.github.com>
2021-09-16 08:05:02 +0300

    Add files via upload

--- home/views.py
+++ home/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request, *args, **kwargs):
+    return render(request, 'home.html', {})
+
+
+def about(request, *args, **kwargs):
+    return render(request, 'about.html', {})
+
+
+def contact(request, *args, **kwargs):
+    return render(request, 'contact.html', {})
+
+
+def services(request, *args, **kwargs):
+    return render(request, 'services.html', {})
