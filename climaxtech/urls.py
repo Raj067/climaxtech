0a98f267f84a07ef08457b1e4e7d4ebc8a852717
Rajabu-Mrisho <rajabumrisho69@gmail.com>
2021-09-16 08:07:07 +0300

    codes

--- climaxtech/urls.py
+++ climaxtech/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from home.views import *
-from home.views import h
 
 urlpatterns = [
     path('', home, name='home'),
